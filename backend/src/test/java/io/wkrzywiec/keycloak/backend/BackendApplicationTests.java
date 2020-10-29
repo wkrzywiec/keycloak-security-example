329c03409e856293e4b2e6f89d7a675baedbc68c
Wojtek Krzywiec <wojciechkrzywiec@gmail.com>
2020-10-29 07:30:59 +0100

    add backend service

---
+++ backend/src/test/java/io/wkrzywiec/keycloak/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package io.wkrzywiec.keycloak.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
