367ed3b98e2304b4efcd39a832a535dd81c76b56
Wojtek Krzywiec <wojciechkrzywiec@gmail.com>
2020-11-07 14:50:07 +0100

    more advanced security config

--- backend/src/main/java/io/wkrzywiec/keycloak/backend/config/SecurityConfig.java
+++ backend/src/main/java/io/wkrzywiec/keycloak/backend/config/SecurityConfig.java
@@ -1,23 +1,34 @@
 package io.wkrzywiec.keycloak.backend.config;
 
-import org.springframework.http.HttpMethod;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(jsr250Enabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Value("${spring.security.ignored}")
+    private String[] nonSecureUrl;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.cors()
+        http
+                .csrf().disable()
+                .cors()
+                .and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                     .authorizeRequests()
-                        .antMatchers(HttpMethod.GET, "/actuator/**")
-                        .permitAll()
+                        .antMatchers(nonSecureUrl).permitAll()
                 .anyRequest().authenticated()
                 .and()
                     .oauth2ResourceServer()
                         .jwt();
+
     }
 }
