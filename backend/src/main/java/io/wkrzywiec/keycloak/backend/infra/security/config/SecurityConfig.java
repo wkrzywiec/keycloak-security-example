0acaff299b5539a7768d54dbe9d37eddf230c953
Wojtek Krzywiec <wojciechkrzywiec@gmail.com>
2021-01-21 09:51:52 +0100

    disable security for some urls

--- backend/src/main/java/io/wkrzywiec/keycloak/backend/infra/security/config/SecurityConfig.java
+++ backend/src/main/java/io/wkrzywiec/keycloak/backend/infra/security/config/SecurityConfig.java
@@ -12,6 +12,7 @@ import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -33,20 +34,22 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .addFilterBefore(
-                        new JwtTokenFilter(
-                                jwtTokenValidator(keycloakJwkProvider()),
-                                authenticationManagerBean(),
-                                authenticationFailureHandler()),
-                        BasicAuthenticationFilter.class)
                 .csrf().disable()
                 .cors()
                 .and()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
-                .authorizeRequests()
-                .antMatchers(nonSecureUrl).permitAll()
-                .anyRequest().authenticated();
+                .addFilterBefore(
+                        new JwtTokenFilter(
+                                jwtTokenValidator(keycloakJwkProvider()),
+                                authenticationManagerBean(),
+                                authenticationFailureHandler()),
+                        BasicAuthenticationFilter.class);
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers(nonSecureUrl);
     }
 
     @ConditionalOnMissingBean
