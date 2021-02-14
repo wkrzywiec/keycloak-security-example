34d77d01ed2cf574a56f136a84206b16902ad346
Wojtek Krzywiec <wojciechkrzywiec@gmail.com>
2021-02-14 12:51:38 +0100

    remove unnecessary code from guard

--- frontend/src/app/guard/auth.guard.ts
+++ frontend/src/app/guard/auth.guard.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
 import { KeycloakAuthGuard, KeycloakService } from 'keycloak-angular';
-import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -19,22 +18,12 @@ export class AuthGuard extends KeycloakAuthGuard {
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Promise<boolean | UrlTree> {
     
-      // Force the user to log in if currently unauthenticated.
     if (!this.authenticated) {
       await this.keycloak.login({
         redirectUri: window.location.origin + state.url,
       });
     }
 
-    // Get the roles required from the route.
-    const requiredRoles = route.data.roles;
-
-    // Allow the user to to proceed if no additional roles are required to access the route.
-    if (!(requiredRoles instanceof Array) || requiredRoles.length === 0) {
-      return true;
-    }
-
-    // Allow the user to proceed if all the required roles are present.
-    return requiredRoles.every((role) => this.roles.includes(role));
+    return this.authenticated;
   }
 }
